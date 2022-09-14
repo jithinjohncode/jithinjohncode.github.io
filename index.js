5b229e7ab1991347fff8772a215dbc4f0d724e83
jithinjohncode <jithinjohncode@gmail.com>
2022-09-14 15:07:01 +0530

    javascript code counter

---
+++ index.js
@@ -0,0 +1,27 @@
+let add = document.querySelector("#add");
+let subract = document.querySelector("#subtract");
+
+console.log(add, subract);
+
+add.addEventListener("click", function () {
+  let output = document.querySelector("#output");
+  let result = Number(output.innerText )+ 1;
+  if (result > 10) {
+    /* code to run if condition is true */
+    result = 0;
+  } 
+output.innerText = result;
+});
+
+subract.addEventListener("click", function () {
+  let output = document.querySelector("#output");
+  let result = Number(output.innerText) - 1;
+
+  if (result < 0) {
+    result = 0;
+  }
+
+  output.innerText = result;
+});
+
+ 
